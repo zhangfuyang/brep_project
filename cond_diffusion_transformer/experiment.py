c763f9ffd1ef3b968a3a43846da67730431751f2
Fuyang Zhang <fuyang.zhang97@gmail.com>
2024-02-26 13:01:32 -0800

    fuck bug

--- cond_diffusion_transformer/experiment.py
+++ cond_diffusion_transformer/experiment.py
@@ -230,7 +230,7 @@ class DiffusionExperiment(pl.LightningModule):
     
     def on_test_end(self) -> None:
         # call data_render_mc.py
-        command = f'python data_rendering_mc.py --data_root {self.logger.log_dir} --output test'
+        command = f'python data_rendering_mc.py --data_root {self.logger.log_dir} --folder_name test'
         print(f'now running {command}')
         os.system(command)
 
